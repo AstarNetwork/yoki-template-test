25467475132be3a484d2c31cb600807e97a6cffa
Mar.io <34627453+Maar-io@users.noreply.github.com>
2024-02-29 19:31:33 +0100

    added real partner NFT

--- test/Partner.t.sol
+++ test/Partner.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {PartnerNFT} from "../src/PartnerNFT.sol";
-// import  "forge-std/Vm.sol";
 
 contract PartnerNFTTest is Test {
     PartnerNFT public partnerNFT;
@@ -27,6 +26,36 @@ contract PartnerNFTTest is Test {
         address someUser = address(1234);
         vm.prank(someUser);
         partnerNFT.mint(someUser, 1);
+    }
+
+    function test_TokenUri() public {
+        // alice mints 1 token
+        address alice = address(1234);
+        partnerNFT.grantRole(partnerNFT.MINTER_ROLE(), address(this));
+        partnerNFT.mint(alice, 1);
+
+        // set BAseUri
+        string memory baseURI = "ipfs://example.com/";
+        partnerNFT.setBaseURI(baseURI);
+        // get the token URI
+        string memory uri = partnerNFT.tokenURI(1);
+    
+
+        assertEq(uri, string.concat(baseURI, "1.json"), "unexpected token URI");
+    }
+
+    function testFails_TokenUriNotExist() public {
+        // alice mints 1 token
+        address alice = address(1234);
+        partnerNFT.grantRole(partnerNFT.MINTER_ROLE(), address(this));
+        partnerNFT.mint(alice, 1);
+
+        // set BAseUri
+        string memory baseURI = "ipfs://example.com/";
+        partnerNFT.setBaseURI(baseURI);
+        uint256 supply = partnerNFT.maxSupply();
+        // get the token URI
+        string memory _uri = partnerNFT.tokenURI(supply+1);
     }
 
 }
