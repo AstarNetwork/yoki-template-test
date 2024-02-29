5b2ab405f3d6a6c233380100d6cbefcaee20e294
Mar.io <34627453+Maar-io@users.noreply.github.com>
2024-02-29 19:00:42 +0100

    initial

--- test/Partner.t.sol
+++ test/Partner.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {PartnerNFT} from "../src/PartnerNFT.sol";
+// import  "forge-std/Vm.sol";
 
 contract PartnerNFTTest is Test {
     PartnerNFT public partnerNFT;
@@ -14,6 +15,18 @@ contract PartnerNFTTest is Test {
     function test_MinterRole() public {
         partnerNFT.grantRole(partnerNFT.MINTER_ROLE(), address(this));
         assertEq(partnerNFT.hasRole(partnerNFT.MINTER_ROLE(), address(this)), true);
+    }
+
+    function test_MintOne() public {
+        address alice = makeAddr("alice");
+        partnerNFT.grantRole(partnerNFT.MINTER_ROLE(), address(this));
+        partnerNFT.mint(alice, 1);
+    }
+
+    function testFails_UnauthorisedMinter() public {
+        address someUser = address(1234);
+        vm.prank(someUser);
+        partnerNFT.mint(someUser, 1);
     }
 
 }
